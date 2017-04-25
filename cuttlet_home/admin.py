771cd1e63342cd4efe523faaf21a530ad18df6cd
Cuttlefysh <dev.cuttlefysh@gmail.com>
2017-04-24 22:34:33 -0500

    Sign in and user profile addition

--- cuttlet_home/admin.py
+++ cuttlet_home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'juice_ml', 'all_juice')
+    list_filter = ['all_juice']
+    search_fields = ['user']
+
+admin.site.register(Profile, ProfileAdmin)
--- cuttlet_home/models.py
+++ cuttlet_home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    juice_ml = models.IntegerField(default=11)
+    all_juice = models.IntegerField(default=11)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, juice_ml=10, all_juice=10)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
